82912dcab018cf778188571f17b418fc1e059a72
Emily Gunn <emilyg5@byui.edu>
2025-09-19 13:15:35 -0600

    C-Sharp prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter = "None";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            // Console.WriteLine("You have an A");
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            // Console.WriteLine("You have a B");
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            // Console.WriteLine("You have a D");
+        }
+        else
+        {
+            letter = "F";
+            // Console.WriteLine("You have an F");
+        }
+        Console.WriteLine($"Your grade: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congradulations! You have passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the class. Do better.");
+        }
     }
 }
