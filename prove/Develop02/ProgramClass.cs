f169bbb005e5f22e7a0de2119758b1f606fbbfca
Emily Gunn <emilyg5@byui.edu>
2025-10-14 10:13:53 -0600

    Journal Program - Fixed attribute names

--- prove/Develop02/ProgramClass.cs
+++ prove/Develop02/ProgramClass.cs
@@ -2,8 +2,8 @@ using System.Security.Cryptography.X509Certificates;
 
 public class ProgramClass
 {
-    public string response = "0";
-    public Journal journal = new Journal();
+    public string _response = "0";
+    public Journal _journal = new Journal();
     public void Menu()
     {
 
@@ -17,38 +17,38 @@ public class ProgramClass
             Console.WriteLine("4. Save");
             Console.WriteLine("5. Quit");
             Console.Write("What would you like to do? ");
-            response = Console.ReadLine();
+            _response = Console.ReadLine();
 
-            if (response == "1")//Write entry
+            if (_response == "1")//Write entry
             {
-                journal.StartEntry();
-                response = "0";
+                _journal.StartEntry();
+                _response = "0";
             }
-            else if (response == "2")//Display entries
+            else if (_response == "2")//Display entries
             {
-                journal.DisplayEntries();
-                response = "0";
+                _journal.DisplayEntries();
+                _response = "0";
             }
-            else if (response == "3")//Load
+            else if (_response == "3")//Load
             {
-                journal.LoadFile();
-                response = "0";
+                _journal.LoadFile();
+                _response = "0";
             }
-            else if (response == "4")//Save
+            else if (_response == "4")//Save
             {
-                journal.SaveToFile();
-                response = "0";
+                _journal.SaveToFile();
+                _response = "0";
             }
-            else if (response == "5")//Quit
+            else if (_response == "5")//Quit
             {
-                response = "";
+                _response = "";
             }
             else
             {
                 Console.WriteLine("Invalid Respose.");
-                response = "0";
+                _response = "0";
             }
 
-        } while ((response == "0"));
+        } while ((_response == "0"));
     }
 }
