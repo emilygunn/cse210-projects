699d2a6d96d23d9d5db093dfcee20f09c8546323
Emily Gunn <emilyg5@byui.edu>
2025-10-14 17:59:48 -0600

    Learning 03 - Fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Xml;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6, 7);
+        Fraction f4 = new Fraction(1, 3);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        f2.SetTop(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        f3.SetTop(3);
+        f3.SetBottom(4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
