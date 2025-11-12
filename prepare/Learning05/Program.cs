0b6599dac38e6cfc6ea9913e79f44ddf303b670a
Emily Gunn <emilyg5@byui.edu>
2025-11-12 16:44:19 -0700

    W09 Prepare - Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("red", 2);
+        // Console.WriteLine(s1.GetColor());
+        // Console.WriteLine(s1.GetArea());
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("orange", 2, 4);
+        // Console.WriteLine(r1.GetColor());
+        // Console.WriteLine(r1.GetArea());
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("yellow", 2);
+        // Console.WriteLine(c1.GetColor());
+        // Console.WriteLine(c1.GetArea());
+        shapes.Add(c1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
